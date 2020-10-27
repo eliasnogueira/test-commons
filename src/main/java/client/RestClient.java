ccc546d57ddd4f4c5b81c35a3aad9fa11db74d96
eliasnogueira <elias.nogueira@gmail.com>
2020-10-27 21:53:51 +0100

    initial test-commons

---
+++ src/main/java/client/RestClient.java
@@ -0,0 +1,60 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Elias Nogueira
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package client;
+
+import static io.restassured.RestAssured.enableLoggingOfRequestAndResponseIfValidationFails;
+import static io.restassured.config.JsonConfig.jsonConfig;
+import static io.restassured.path.json.config.JsonPathConfig.NumberReturnType.BIG_DECIMAL;
+
+import io.restassured.config.RestAssuredConfig;
+
+public class RestClient {
+
+    public static final String SLASH = "/";
+
+    private final String baseUri;
+    private final String basePath;
+    private final String port;
+    private final String version;
+
+    public RestClient(String baseUri, String port, String basePath, String version) {
+        getRestAssuredConfig();
+
+        this.baseUri = baseUri;
+        this.basePath = basePath;
+        this.port = port;
+        this.version = version;
+    }
+
+    public String getPath(String resource) {
+        return baseUri + ":" + port + basePath + version + resource;
+    }
+
+    public void getRestAssuredConfig() {
+        RestAssuredConfig.newConfig().
+            jsonConfig(jsonConfig().numberReturnType(BIG_DECIMAL));
+
+        enableLoggingOfRequestAndResponseIfValidationFails();
+    }
+}
---
+++ src/main/java/config/ConfigurationManager.java
@@ -0,0 +1,12 @@
+package config;
+
+import org.aeonbits.owner.ConfigCache;
+
+public class ConfigurationManager {
+
+    private ConfigurationManager() {}
+
+    public static Configuration getConfiguration() {
+        return ConfigCache.getOrCreate(Configuration.class);
+    }
+}
